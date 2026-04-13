c94fa79fa3d68723286a62006a65baa149e5a568
feaslylab <feaslylab@users.noreply.github.com>
2026-04-13 12:34:52 +0000

    deploy: 4c7bca42f8746fe7d87a474d90a06a991aa22a19

---
+++ assets/user-b8dd1f14.js
@@ -0,0 +1,11 @@
+import{c}from"./index-b33d5033.js";/**
+ * @license lucide-react v0.310.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("Tag",[["path",{d:"M12 2H2v10l9.29 9.29c.94.94 2.48.94 3.42 0l6.58-6.58c.94-.94.94-2.48 0-3.42L12 2Z",key:"14b2ls"}],["path",{d:"M7 7h.01",key:"7u93v4"}]]);/**
+ * @license lucide-react v0.310.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=c("User",[["path",{d:"M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2",key:"975kel"}],["circle",{cx:"12",cy:"7",r:"4",key:"17ys0d"}]]);export{a as T,s as U};
